ce1c33736d818d67a04055c7856b69126a29d8b8
shardanov <shardanow@gmail.com>
2024-02-16 15:06:53 +0200

    Logger small refactoring. Commented signal handler logic on main.

--- include/utils/Logger.hpp
+++ include/utils/Logger.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <mutex>
 #include <iomanip>
@@ -13,4 +12,8 @@ public:
     //TODO - make message receive not only string
     void logError(const std::string& message, const std::string& color = RED); 
     std::string getCurrentTimestamp();
+
+private:
+    std::mutex logger_mutex_;
+    std::mutex timeMutex;
 };
